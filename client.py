cd660448f05fd9ab7fd74f2e4fb079c2d2f44f5f
m-trinacty <m.trinacty@email.cz>
2022-04-12 00:21:17 +0200

    Functionality working

---
+++ client.py
@@ -0,0 +1,48 @@
+from curses.ascii import NUL
+import socket
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+class Client(QObject):
+    def __init__(self):
+        super(Client, self).__init__()
+        self.IP = "192.168.151.1"
+        self.PORT = 1500
+        self.ADDR = (self.IP, self.PORT)
+        self.SIZE = 1024
+        self.FORMAT = "ISO-8859-1"
+        self.client = NUL
+
+    def createClient(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.ADDR)
+        self.connect = True
+
+        
+            
+        
+    def closeClient(self):
+        print("Connection ended")
+        self.client.close()
+    
+
+    def sendMessage(self,msg):
+        print(f"Sending: {msg}")
+        #msg = str(input("InputMesaage:"))
+        strLen=len(msg)
+        sendMsg= list(msg)
+        for x in range(strLen,128):
+            sendMsg.append('\0')
+        self.client.send(bytes(msg,self.FORMAT))    
+
+    
+    def readIncome(self):
+        
+
+        while self.connect:
+            
+            data = self.client.recv(self.SIZE)
+
+            serverMsg= data.decode(self.FORMAT)
+            serverMsg = serverMsg.split('\0')
+            print(f"[SERVER] {serverMsg[0]}")
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from PyQt5 import QtWidgets, uic
-import sys
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox,QAction, QToolTip, QStackedWidget, QHBoxLayout, QVBoxLayout, QSplitter, QFormLayout, QLabel, QFrame, QPushButton, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QWidget, QHeaderView
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor, QFont, QIcon
+from PyQt5.QtCore import Qt, QPoint, QRect, QObject, QEvent, pyqtSignal, pyqtSlot, QSize, QDir
 
+import sys
+import socket
+from client import Client
+from threading import *
 
 class Ui(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('form.ui', self)
+        self.ui = uic.loadUi('form.ui', self)
         self.setButtons()
         self.createStatusBar()
         self.show()
+        self.cl = Client()
+        self.cl.createClient()
+        self.thread()
+        quit = QAction("Quit", self)
+        quit.triggered.connect(self.closeEvent)
 
 
     def setButtons(self):
@@ -20,11 +32,33 @@ class Ui(QtWidgets.QMainWindow):
 
     def stopButtonClicked(self):
         print("stop button clicked")
+        self.cl.sendMessage("STOP")
     def startButtonClicked(self):
         print("start button clicked")
+        self.cl.sendMessage("START 55 120")
 
     def createStatusBar(self):
         self.statusBar().showMessage("Ready")
+    def thread(self):
+        self.t1=Thread(target=self.cl.readIncome)
+        self.t1.start()
+        
+
+    def closeEvent(self, event):
+        
+        reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the window?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+            self.cl.sendMessage("STOP")
+            self.cl.sendMessage("QUIT")
+            self.cl.connect=False
+            self.t1.join()
+            self.t1.terminate()
+            self.cl.closeClient()
+            print('Window closed')
+        else:
+            event.ignore()
+
 
 
 if __name__ == "__main__":
